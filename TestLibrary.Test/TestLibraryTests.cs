6192406b21101e4534eb098e9e31ed207de7e7e7
Zoltán Tóth <zshun86@gmail.com>
2018-03-21 11:06:50 +0100

    Library sources added

---
+++ TestLibrary.Test/TestLibraryTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TestLibrary.Test
+{
+    [TestFixture]
+    public class TestLibraryTests
+    {
+        [Test]
+        public void Two_plus_two_should_be_four()
+        {
+            var subject = new TestLibrary();
+            Assert.That(subject.Add(2, 2), Is.EqualTo(4));
+        }
+    }
+}
---
+++ TestLibrary/TestLibrary.cs
@@ -0,0 +1,10 @@
+﻿namespace TestLibrary
+{
+    public class TestLibrary
+    {
+        public int Add(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
